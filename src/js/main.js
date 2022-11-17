65ce7a024456cb60d303a9cf87535a1950cfa490
Hazey8709 <jfortin802@gmail.com>
2022-11-17 18:12:24 -0500

    updated files

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+import userProfile from "./data/user";
+//import name from "./data/user";
+
+const userSection = document.getElementById("user");
+const userInfoList = document.createElement("ul");
+
+const buildUser = (userData) => {
+    for (const prop in userData) {
+        const lineItemContent = `${prop}: ${userData[prop]}`;
+        const lineItem = document.createElement("li");
+        lineItem.innerText = lineItemContent;
+        userInfoList.appendChild(lineItem);
+        userSection.appendChild(userInfoList);
+    }
+};
+
+(() => {
+    //console.log("working!");
+    //console.log("Hi:", name);
+    console.log(userProfile);
+    buildUser(userProfile);
+})();
