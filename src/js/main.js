3e55dba34acb0bc7d630522452596bb89550425f
Hazey8709 <jfortin802@gmail.com>
2022-11-18 19:28:43 -0500

    final update

--- src/js/main.js
+++ src/js/main.js
@@ -5,22 +5,53 @@ import mapboxgl from "mapbox-gl";
 const userSection = document.getElementById("user");
 const userInfoList = document.createElement("ul");
 
-const buildUser = (userData) => {
-    for (const prop in userData) {
-        const lineItemContent = `${prop}: ${userData[prop]}`;
-        const lineItem = document.createElement("li");
-        lineItem.innerText = lineItemContent;
-        userInfoList.appendChild(lineItem);
-        userSection.appendChild(userInfoList);
-    }
+const buildAvatar = (imgsrc, parent) => {
+    const imageElement = document.createElement("img");
+    imageElement.src = imgsrc;
+    parent.appendChild(imageElement);
 };
 
-(() => {
-    //console.log("working!");
-    //console.log("Hi:", name);
-    console.log(userProfile);
-    buildUser(userProfile);
+const buildName = (data, parent) => {
+    const H2 = document.createElement("h2");
+    const SPAN = document.createElement("span");
+    H2.innerText = data;
+    SPAN.innerText = "where in the world is";
 
+    parent.appendChild(H2);
+    H2.insertAdjacentElement("afterbegin", SPAN);
+};
+
+const buildEmail = (data, parent) => {
+    const link = document.createElement("a");
+    link.innerText = data;
+    link.href = `mailto:${data}`;
+
+    parent.appendChild(link);
+};
+
+const buildLocation = (data, parent) => {
+    const p = document.createElement("p");
+    p.innerText = `longitude: ${data[0]}, latitude: ${data[1]}`;
+
+    parent.appendChild(p);
+};
+
+const buildUser = (userData) => {
+    buildAvatar(userData.avatar, userSection);
+    buildName(userData.name, userSection);
+    buildEmail(userData.email, userSection);
+    buildLocation([userData.longitude, userData.latitude], userSection);
+
+    // for (const prop in userData) {
+    //     const lineItemContent = `${prop}: ${userData[prop]}`;
+    //     const lineItem = document.createElement("li");
+    //     lineItem.innerText = lineItemContent;
+    //     userInfoList.appendChild(lineItem);
+    //     userSection.appendChild(userInfoList);
+    // }
+};
+
+const buildMap = (long, lat) => {
     mapboxgl.accessToken =
         process.env.NODE_ENV === "production"
             ? process.env.API_KEY_MAPBOX
@@ -28,10 +59,18 @@ const buildUser = (userData) => {
     const map = new mapboxgl.Map({
         container: "mapContainer", // container ID
         style: "mapbox://styles/hazey87/clalzmyui000j15qr0pb7cicy", // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9, // starting zoom
+        center: [long, lat], // starting position [lng, lat]
+        zoom: 0, // starting zoom
         projection: "globe", // display the map as a 3D globe
     });
+};
+
+(() => {
+    //console.log("working!");
+    //console.log("Hi:", name);
+    //console.log(userProfile);
+    buildUser(userProfile);
+    buildMap(userProfile.longitude, userProfile.longitude);
 })();
 // "mapbox://styles/hazey87/clalzmyui000j15qr0pb7cicy", // style URL
 // "mapbox://styles/mapbox/streets-v11", // style URL
